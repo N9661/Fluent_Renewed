7fede0cdd78efb94d6212e222ef33bdb648440c5
Renamed <c4xhavoc@gmail.com>
2025-04-30 07:40:51 -0500

    Create whitelist_data.lua

---
+++ whitelist_data.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        username = "HVX_Havoc",
+        userId = 8367759083,
+        clientId = "fba078e1-e7a1-4082-0000-000000000000",
+        hwid = "e9b170f70b95881d19abdb753e64d6513e354bc72e37c7d45b13b1f36ec3aa60"
+    },
+    {
+        username = "Tester",
+        userId = 12345678,
+        clientId = "98765432-ABCD-11EC-0000-000000000000",
+        hwid = "123456ABCDEF"
+    },
+    -- Add more users as needed
+}
